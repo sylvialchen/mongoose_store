6637b0dfa8d787bf43ee39ed8e63da140799dd7b
Sylvia Chen <sylvialchen33@gmail.com>
2022-09-07 14:51:10 -0400

    edit working

---
+++ controllers/products.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const productRouter = express.Router();
+const Product = require('../models/products.js');
+
+module.exports = productRouter;
+
+// Routes / Controllers
+// Seed
+const productSeed = require('../models/productSeed.js');
+
+productRouter.get('/seed', (req, res) => {
+	Product.deleteMany({}, (error, allProducts) => {});
+
+	Product.create(productSeed, (error, data) => {
+		res.redirect('/products');
+	});
+});
+
+// Index
+productRouter.get('/', (req, res) => {
+	Product.find({}, (error, allProducts) => {
+		res.render('index.ejs', {
+			products: allProducts,
+		});
+	});
+});
+// New
+productRouter.get('/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// Delete
+productRouter.delete("/:id", (req, res) => {
+    Product.findByIdAndDelete(req.params.id, (err, data) => {
+      res.redirect("/products")
+    })
+  });
+
+// Update
+productRouter.put("/:id", (req, res) => {  
+    Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      },
+      (error, updatedProduct) => {
+        res.redirect(`./${req.params.id}`)
+      }
+    )
+  })
+
+// Create
+productRouter.post('/', (req, res) => {
+    Product.create(req.body, (error, productBook) => {
+        res.redirect('/products');
+    });
+});
+
+// Edit
+productRouter.get("/:id/edit", (req, res) => {
+    Product.findById(req.params.id, (err, foundProduct) => {
+      res.render("edit.ejs", {
+        product: foundProduct,
+      })
+    })
+  })
+
+// Show
+productRouter.get("/:id", (req, res) => {
+	Product.findById(req.params.id, (err, foundProduct) => {
+		res.render("show.ejs", {
+            product: foundProduct,
+        })
+	});
+});
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const express = require('express');
 const app = express();
 const mongoose = require ('mongoose');
 const db = mongoose.connection;
-const Product = require('./models/products');
+const methodOverride = require("method-override");
+const productsController = require('./controllers/products.js');
 
 //Port
 //___________________
@@ -28,56 +29,10 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongod connected: to MONGO_URI'));
 db.on('disconnected', () => console.log('mongod disconnected'));
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
-
-// Routes / Controllers
-// Seed
-const productSeed = require('./models/productSeed.js');
-
-app.get('/products/seed', (req, res) => {
-	Product.deleteMany({}, (error, allProducts) => {});
-
-	Product.create(productSeed, (error, data) => {
-		res.redirect('/products');
-	});
-});
-
-// Index
-app.get('/products', (req, res) => {
-	Product.find({}, (error, allProducts) => {
-		res.render('index.ejs', {
-			products: allProducts,
-		});
-	});
-});
-// New
-app.get('/products/new', (req, res) => {
-	res.render('new.ejs');
-});
-// Delete
-// Update
-// Create
-app.post('/products', (req, res) => {
-    Product.create(req.body, (error, createdProduct) => {
-    res.redirect('/products');
-});
-});
-// Edit
-app.get("/products/:index/edit", (req, res) => {
-	res.render("edit.ejs", {
-
-    })
-});
-
-// Show
-app.get("/products/:id", (req, res) => {
-	Product.findById(req.params.id, (err, foundProduct) => {
-		res.render("show.ejs", {
-            product: foundProduct,
-        })
-	});
-});
-
+app.use(methodOverride("_method"));
+app.use('/products', productsController);
 
 // Listener
 app.listen(PORT, () => console.log(`server is listning on port: ${PORT}`));
